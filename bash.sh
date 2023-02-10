5bff1fdb9ec4886d46feb318f0be9f1f41e854c9
RohanRusta21 <Rohanrustagi21@gmail.com>
2023-02-10 14:58:32 +0530

    1 Commit

--- bash.sh
+++ bash.sh
@@ -11,9 +11,18 @@ git add .
 for i in {1..100}
 do
 	git commit -m ""${i}" Commit"
-	echo ${i}
+	#echo ${i}
 
 done
+
+git remote add origin ${url}
+
+read -p "which branch you want to create :" branch
+git branch ${branch}
+
+git checkout ${branch}
+
+git push -u origin ${branch}
 
 
 
